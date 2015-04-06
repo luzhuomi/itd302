37728b2bd184408b38cd59b77a5045b8bb04fa9f
luzhuomi <luzhuomi@gmail.com>
2015-04-06 14:46:38 +0800

    added pratical 6

---
+++ practical6/src/main/java/itd302/pratical6/UDFRemoveLink.java
@@ -0,0 +1,26 @@
+package itd302.practical6;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+@Description(name = "removelink", 
+	value = "_FUNC_(text) - remove the links of pattern 'http://...' from the text.")
+
+public class UDFRemoveLink extends UDF
+{
+
+	private Pattern pattern;
+
+	public UDFRemoveLink() 
+	{
+		this.pattern = new Pattern.compile("http(:?s?)://[a-zA-Z0-9\\./\\-\\+@]+");
+	};
+
+	public String evaluate(String text) 
+	{
+		Matcher m = this.pattern.matcher(text);
+		return m.replaceAll("");
+	}
+
+}
